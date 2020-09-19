d3567f7e6eb9e9017cde45c6fc27de78f5b7d472
VINCENT ADENIJI <54925113+TOSINNIJIS1@users.noreply.github.com>
2020-09-18 23:26:57 -0400

    Redone Sprint

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -17,7 +17,19 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
+class GroundVehicle():
+    def __init__(self, num_wheels = 4):
+        self.num_wheels = num_wheels
+
+    def drive(self):
+        return "vroooom"
 # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(2)
+
+    def drive(self):
+        return 'BRAAAP!!'
 
 vehicles = [
     GroundVehicle(),
@@ -30,3 +42,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for engines in vehicles:
+    print(engines.drive())
